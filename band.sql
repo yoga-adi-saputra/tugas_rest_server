25f5ebebc1891500decab0883e49d02264b150ad
yoga-adi-saputra <visectedbdm01@gmail.com>
2020-12-16 20:08:02 +0700

    1618053 new

---
+++ band.sql
@@ -0,0 +1,131 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 16, 2020 at 02:04 PM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `band`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `daftar_band`
+--
+
+CREATE TABLE `daftar_band` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `genre` varchar(50) NOT NULL,
+  `kota` varchar(50) NOT NULL,
+  `kontak` int(13) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `daftar_band`
+--
+
+INSERT INTO `daftar_band` (`id`, `nama`, `genre`, `kota`, `kontak`) VALUES
+(1, 'Metropold', 'Brutal Death Metal', 'Malang', 898838383),
+(2, 'Rotten Corpse', 'Brutal Death Metal', 'Malang', 888888899),
+(3, 'Rottenomicon', 'Death Metal', 'Malang', 85999888),
+(4, 'The Last Fatamorgana', 'Groove/Thrash Metal', 'Batu', 89888888),
+(5, 'Criminal Impact', 'Death Metal', 'Malang', 888888776),
+(6, 'Sluger', 'Grindcore', 'Batu', 866543),
+(7, 'Visected', 'Brutal Death Metal', 'Batu', 898824088);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `keys`
+--
+
+CREATE TABLE `keys` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `key` varchar(40) NOT NULL,
+  `level` int(2) NOT NULL,
+  `ignore_limits` tinyint(1) NOT NULL DEFAULT '0',
+  `is_private_key` tinyint(1) NOT NULL DEFAULT '0',
+  `ip_addresses` text,
+  `date_created` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `keys`
+--
+
+INSERT INTO `keys` (`id`, `user_id`, `key`, `level`, `ignore_limits`, `is_private_key`, `ip_addresses`, `date_created`) VALUES
+(1, 1, '112233', 1, 0, 0, '1', 1),
+(2, 2, '445566', 1, 0, 0, '1', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `limits`
+--
+
+CREATE TABLE `limits` (
+  `id` int(11) NOT NULL,
+  `uri` varchar(255) NOT NULL,
+  `count` int(10) NOT NULL,
+  `hour_started` int(11) NOT NULL,
+  `api_key` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `limits`
+--
+
+INSERT INTO `limits` (`id`, `uri`, `count`, `hour_started`, `api_key`) VALUES
+(1, 'uri:Baju_adat/index:get', 2, 1607878463, '112233'),
+(2, 'method-name:index_get', 2, 1607878672, '112233'),
+(3, 'api-key:112233', 2, 1608121982, '112233'),
+(4, 'api-key:445566', 6, 1608120297, '445566');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `keys`
+--
+ALTER TABLE `keys`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `limits`
+--
+ALTER TABLE `limits`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `keys`
+--
+ALTER TABLE `keys`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `limits`
+--
+ALTER TABLE `limits`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
